b200a18e9e09ba3c961bf1e148d1ac76e7265c34
Yan <yavallejor@gmail.com>
2021-05-11 23:31:53 -0500

    Initial Structure Plugin

---
+++ testimonialsblocks.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:       Testimonials Slick
+ * Plugin URI:        https://github.com/yavallejo/testimonialsBlock
+ * Description:       Handle the basics with this plugin.
+ * Version:           0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            @yavallejo
+ * Author URI:        https://www.aprendiendoando.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       frontporchsolutions
+ * Domain Path:       /languages
+ */
